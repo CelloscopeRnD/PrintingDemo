b634aceb118a755aeae7b363bec89577097268da
zamanAEL <assad.zaman@gmail.com>
2015-11-27 20:19:55 +0600

    initial commit

---
+++ app/src/main/java/co/celloscope/printingdemo/MainActivity.java
@@ -0,0 +1,273 @@
+package co.celloscope.printingdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintJob;
+import android.print.PrintManager;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.print.PrintHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int THUMBNAIL_SIZE = 400;
+    private static final int PICK_LOGO = 100;
+    public static final int PICK_PHOTO = 200;
+    private static final int PICK_BARCODE = 300;
+    private List<PrintJob> mPrintJobs;
+    private EditText pinEditText;
+    private EditText nameEditText;
+    private String photoFilePath = "";
+    private String barcodeFilePath = "";
+    private String logoFilePath = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.logoButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_LOGO);
+            }
+        });
+        findViewById(R.id.photoButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_PHOTO);
+            }
+        });
+        findViewById(R.id.barcodeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_BARCODE);
+            }
+        });
+
+
+        pinEditText = (EditText) findViewById(R.id.pinEditText);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+
+        mPrintJobs = new ArrayList<>(10);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    doWebViewPrint();
+//                    startPrintActivity();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            ImageView imageView = null;
+            switch (requestCode) {
+                case PICK_LOGO:
+                    imageView = (ImageView) findViewById(R.id.logoImageView);
+                    logoFilePath = getRealPathFromUri(this, data.getData());
+                    break;
+                case PICK_PHOTO:
+                    imageView = (ImageView) findViewById(R.id.photoImageView);
+                    photoFilePath = getRealPathFromUri(this, data.getData());
+                    break;
+                case PICK_BARCODE:
+                    imageView = (ImageView) findViewById(R.id.barcodeImageView);
+                    barcodeFilePath = getRealPathFromUri(this, data.getData());
+                    break;
+            }
+            if (imageView != null) {
+                final Bitmap photo;
+                try {
+                    photo = getThumbnail(data.getData());
+                    imageView.setImageBitmap(photo);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static String getRealPathFromUri(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    public Bitmap getThumbnail(Uri uri) throws FileNotFoundException, IOException {
+        InputStream input = this.getContentResolver().openInputStream(uri);
+
+        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
+        onlyBoundsOptions.inJustDecodeBounds = true;
+        onlyBoundsOptions.inDither = true;//optional
+        onlyBoundsOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;//optional
+        BitmapFactory.decodeStream(input, null, onlyBoundsOptions);
+        input.close();
+        if ((onlyBoundsOptions.outWidth == -1) || (onlyBoundsOptions.outHeight == -1))
+            return null;
+
+        int originalSize = (onlyBoundsOptions.outHeight > onlyBoundsOptions.outWidth) ? onlyBoundsOptions.outHeight : onlyBoundsOptions.outWidth;
+
+        double ratio = (originalSize > THUMBNAIL_SIZE) ? (originalSize / THUMBNAIL_SIZE) : 1.0;
+
+        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+        bitmapOptions.inSampleSize = getPowerOfTwoForSampleRatio(ratio);
+        bitmapOptions.inDither = true;//optional
+        bitmapOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;//optional
+        input = this.getContentResolver().openInputStream(uri);
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
+        input.close();
+        return bitmap;
+    }
+
+    private static int getPowerOfTwoForSampleRatio(double ratio) {
+        int k = Integer.highestOneBit((int) Math.floor(ratio));
+        if (k == 0) return 1;
+        else return k;
+    }
+
+    private String getHtml() throws IOException {
+        InputStream is = getAssets().open("Template1.html");
+        int size = is.available();
+
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+
+        String str = new String(buffer);
+        String pin = pinEditText.getText().toString();
+        String name = nameEditText.getText().toString();
+        str = str.replace("#LOGO", logoFilePath);
+        str = str.replace("#PHOTO", photoFilePath);
+        str = str.replace("#BARCODE", barcodeFilePath);
+        str = str.replace("#PIN", pin);
+        str = str.replace("#NAME", name);
+        return str;
+    }
+
+    private void startPrintActivity() throws IOException {
+        Intent intent = new Intent("com.sec.print.mobileprint.action.PRINT");
+        Uri uri = Uri.fromFile(getFile());
+        intent.putExtra("com.sec.print.mobileprint.extra.CONTENT", uri);
+
+        intent.putExtra("com.sec.print.mobileprint.extra.CONTENT_TYPE", "WEBPAGE_AUTO");
+        intent.putExtra("com.sec.print.mobileprint.extra.OPTION_TYPE", "DOCUMENT_PRINT");
+        intent.putExtra("com.sec.print.mobileprint.extra.JOB_NAME", "TestPrint");
+        startActivity(intent);
+    }
+
+    private File getFile() throws IOException {
+        File outputDir = this.getExternalCacheDir(); // context being the Activity pointer
+        File temp = File.createTempFile("test", ".html", outputDir);
+        BufferedWriter bw = new BufferedWriter(new FileWriter(temp));
+        bw.write(getHtml());
+        bw.close();
+        return temp;
+    }
+
+    private WebView mWebView;
+
+    private void doWebViewPrint() throws IOException {
+        // Create a WebView object specifically for printing
+//        WebView webView = new WebView(getActivity());
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient() {
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.i(TAG, "page finished loading " + url);
+                createWebPrintJob(view);
+                mWebView = null;
+            }
+        });
+
+        // Generate an HTML document on the fly:
+        String htmlDocument = getHtml();
+        webView.loadDataWithBaseURL(null, htmlDocument, "text/HTML", "UTF-8", null);
+
+        // Keep a reference to WebView object until you pass the PrintDocumentAdapter
+        // to the PrintManager
+        mWebView = webView;
+    }
+
+    private void doPhotoPrint() {
+        PrintHelper photoPrinter = new PrintHelper(this);
+        photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FIT);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.heart);
+        photoPrinter.printBitmap("droids.jpg - test print", bitmap);
+    }
+
+    private void createWebPrintJob(WebView webView) {
+
+        // Get a PrintManager instance
+        PrintManager printManager = (PrintManager) this.getSystemService(Context.PRINT_SERVICE);
+
+        // Get a print adapter instance
+        PrintDocumentAdapter printAdapter = webView.createPrintDocumentAdapter();
+
+        // Create a print job with name and adapter instance
+        String jobName = getString(R.string.app_name) + " Document";
+        PrintJob printJob = printManager.print(jobName, printAdapter,
+                new PrintAttributes.Builder().build());
+
+        // Save the job object for later status checking
+        mPrintJobs.add(printJob);
+    }
+}
